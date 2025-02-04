cc82d9083f578aca6e3e1ec6aa0c487b8d7070e3
SunnyBoy-y <66504476+SunnyBoy-y@users.noreply.github.com>
2025-02-04 22:22:01 +0800

    Update es8388_audio_codec.h
    音量增益设置

--- main/audio_codecs/es8388_audio_codec.h
+++ main/audio_codecs/es8388_audio_codec.h
@@ -32,6 +32,8 @@ public:
     virtual void SetOutputVolume(int volume) override;
     virtual void EnableInput(bool enable) override;
     virtual void EnableOutput(bool enable) override;
+
+    void WriteReg(uint8_t reg_addr, uint8_t data);
 };
 
 #endif // _ES8388_AUDIO_CODEC_H
