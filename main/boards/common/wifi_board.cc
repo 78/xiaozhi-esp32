31bc326542bd9e4556b049dbb4f761e71cf599c8
zczc365 <zczc365@gmail.com>
2025-02-07 22:40:39 +0800

    support wifi smartconfig

--- main/boards/common/wifi_board.cc
+++ main/boards/common/wifi_board.cc
@@ -20,6 +20,10 @@
 #include <wifi_configuration_ap.h>
 #include <ssid_manager.h>
 
+#include "lcd_display.h"
+
+#define WIFI_SMARTCONFIG_URL	"https://iot.espressif.cn/configWXDeviceWiFi.html"
+
 static const char *TAG = "WifiBoard";
 
 WifiBoard::WifiBoard() {
@@ -46,11 +50,19 @@ void WifiBoard::EnterWifiConfigMode() {
 
     // 显示 WiFi 配置 AP 的 SSID 和 Web 服务器 URL
     std::string hint = "请在手机上连接热点 ";
+    if (display->GetType() == Display::DisplayType::LCD) {
+        hint = "微信直接扫二维码配网，或在手机上连接热点 ";
+    }
     hint += wifi_ap.GetSsid();
     hint += "，然后打开浏览器访问 ";
     hint += wifi_ap.GetWebServerUrl();
 
     display->SetStatus(hint);
+
+    // 如果是LCD显示屏则显示微信配网二维码
+    if (display->GetType() == Display::DisplayType::LCD) {
+        static_cast<LcdDisplay*>(display)->lv_smartconfig_page(WIFI_SMARTCONFIG_URL);
+    }    
     
     // 播报配置 WiFi 的提示
     application.Alert("Info", "进入配网模式");
