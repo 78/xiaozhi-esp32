6c609b9a671a704745463f6fea0d5a1fa9e79876
zczc365 <zczc365@gmail.com>
2025-02-07 22:28:31 +0800

    support multiple_clicks

--- main/boards/common/button.cc
+++ main/boards/common/button.cc
@@ -4,14 +4,14 @@
 
 static const char* TAG = "Button";
 
-Button::Button(gpio_num_t gpio_num, bool active_high) : gpio_num_(gpio_num) {
+Button::Button(gpio_num_t gpio_num, bool active_high, uint16_t short_press_time) : gpio_num_(gpio_num) {
     if (gpio_num == GPIO_NUM_NC) {
         return;
     }
     button_config_t button_config = {
         .type = BUTTON_TYPE_GPIO,
         .long_press_time = 1000,
-        .short_press_time = 50,
+        .short_press_time = short_press_time,
         .gpio_button_config = {
             .gpio_num = gpio_num,
             .active_level = static_cast<uint8_t>(active_high ? 1 : 0)
@@ -93,4 +93,43 @@ void Button::OnDoubleClick(std::function<void()> callback) {
             button->on_double_click_();
         }
     }, this);
+}
+
+void Button::OnThreeClick(std::function<void()> callback) {
+    if (button_handle_ == nullptr) {
+        return;
+    }
+    on_three_click_ = callback;
+    button_event_config_t btn_cfg;
+    btn_cfg.event = BUTTON_MULTIPLE_CLICK;
+    btn_cfg.event_data.multiple_clicks.clicks = 3;
+    iot_button_register_event_cb(button_handle_, btn_cfg, [](void* handle, void* usr_data) {
+        Button* button = static_cast<Button*>(usr_data);
+        if (button->on_three_click_) {
+            button->on_three_click_();
+        }
+    }, this);
+}
+
+void Button::OnFourClick(std::function<void()> callback) {
+    if (button_handle_ == nullptr) {
+        return;
+    }
+    on_four_click_ = callback;
+    button_event_config_t btn_cfg;
+    btn_cfg.event = BUTTON_MULTIPLE_CLICK;
+    btn_cfg.event_data.multiple_clicks.clicks = 4;
+    iot_button_register_event_cb(button_handle_, btn_cfg, [](void* handle, void* usr_data) {
+        Button* button = static_cast<Button*>(usr_data);
+        if (button->on_four_click_) {
+            button->on_four_click_();
+        }
+    }, this);
+}
+
+int Button::getButtonLevel() const {
+    if (gpio_num_ == GPIO_NUM_NC) {
+        return -1;
+    }
+    return gpio_get_level(gpio_num_);
 }
