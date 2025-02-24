d40867388ed67db16b44a460dd99e45b81d454cf
Xiaoxia <terrence@tenclass.com>
2025-02-24 18:24:07 +0800

    Update board_control.cc
    不要提交没有使用的功能到main分支

--- main/boards/esp32-s3-touch-amoled-1.8/board_control.cc
+++ main/boards/esp32-s3-touch-amoled-1.8/board_control.cc
@@ -1,30 +1,20 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/timers.h>
+#include <freertos/task.h>
+#include <esp_log.h>
+
 #include "board.h"
 #include "boards/common/wifi_board.h"
-#include "iot/thing.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/timers.h"
-#include "freertos/task.h"
-#include <esp_log.h>
-#define TAG "BoardControl"
 #include "boards/esp32-s3-touch-amoled-1.8/config.h"
+#include "iot/thing.h"
+
+#define TAG "BoardControl"
 
 namespace iot {
 
 class BoardControl : public Thing {
-private:
-    TimerHandle_t sleep_timer_;  // 添加定时器句柄
-
-    // 添加定时器回调函数
-    // static void SleepTimerCallback(TimerHandle_t xTimer) {
-    //     ESP_LOGI(TAG, "System entering sleep mode after delay");
-    //     Board::GetInstance().Sleep();
-    // }
-
 public:
     BoardControl() : Thing("BoardControl", "当前 AI 机器人管理和控制") {
-        // 创建单次定时器
-        // sleep_timer_ = xTimerCreate("SleepTimer", pdMS_TO_TICKS(5000), pdFALSE, this, SleepTimerCallback);
-        
         // 添加电池电量属性
         properties_.AddNumberProperty("BatteryLevel", "当前电池电量百分比", [this]() -> int {
             int level = 0;
@@ -43,14 +33,6 @@ public:
             return charging;
         });
 
-        // 修改休眠方法
-        // methods_.AddMethod("Sleep", "进入关机/休眠状态", ParameterList(), 
-        //     [this](const ParameterList& parameters) {
-        //         ESP_LOGI(TAG, "Delaying sleep for 5 seconds");
-        //         if (sleep_timer_ != NULL) {
-        //             xTimerStart(sleep_timer_, 0);  // 启动定时器
-        //         }
-        //     });
         // 修改重新配网
         methods_.AddMethod("ResetWifiConfiguration", "重新配网", ParameterList(), 
             [this](const ParameterList& parameters) {
@@ -61,13 +43,6 @@ public:
                 }
             });
     }
-    
-    // 添加析构函数释放定时器资源
-    // ~BoardControl() {
-    //     if (sleep_timer_ != NULL) {
-    //         xTimerDelete(sleep_timer_, 0);
-    //     }
-    // }
 };
 
 } // namespace iot
