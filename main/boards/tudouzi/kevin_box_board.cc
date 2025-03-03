6318dd17930f04432f9451513ff5866e6b7e2a8d
Terrence <terrence@tenclass.com>
2025-03-04 07:56:07 +0800

    IoT电池状态读取

--- main/boards/tudouzi/kevin_box_board.cc
+++ main/boards/tudouzi/kevin_box_board.cc
@@ -207,6 +207,7 @@ private:
     void InitializeIot() {
         auto& thing_manager = iot::ThingManager::GetInstance();
         thing_manager.AddThing(iot::CreateThing("Speaker"));
+        thing_manager.AddThing(iot::CreateThing("Battery"));
     }
 
 public:
