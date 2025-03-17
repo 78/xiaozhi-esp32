38f64d1eb9f8cbd36a6c43572a664a70763c8bcf
SunnyBoy-y <66504476+SunnyBoy-y@users.noreply.github.com>
2025-03-18 03:11:33 +0800

    Update atk_dnesp32s3.cc
    dnesp32s3适配微信界面

--- main/boards/atk-dnesp32s3/atk_dnesp32s3.cc
+++ main/boards/atk-dnesp32s3/atk_dnesp32s3.cc
@@ -137,7 +137,11 @@ private:
                                     {
                                         .text_font = &font_puhui_20_4,
                                         .icon_font = &font_awesome_20_4,
-                                        .emoji_font = font_emoji_64_init(),
+                                        #if CONFIG_USE_WECHAT_MESSAGE_STYLE
+                                            .emoji_font = font_emoji_32_init(),
+                                        #else
+                                            .emoji_font = DISPLAY_HEIGHT >= 240 ? font_emoji_64_init() : font_emoji_32_init(),
+                                        #endif
                                     });
     }
 
