7b7f90358a976829fc914145118e7e751c8f1ae5
waahah <3180800798@qq.com>
2025-03-20 03:27:31 +0800

    Bump to 1.5.0

--- main/display/display.h
+++ main/display/display.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: waahah 3180800798@qq.com
+ * @Date: 2025-03-20 02:22:10
+ * @LastEditors: waahah 3180800798@qq.com
+ * @LastEditTime: 2025-03-20 03:27:20
+ * @FilePath: \xiaozhi-esp32\main\display\display.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
@@ -25,8 +33,8 @@ public:
     virtual void SetEmotion(const char* emotion);
     virtual void SetChatMessage(const char* role, const char* content);
     virtual void SetIcon(const char* icon);
-    virtual void SetTheme(const std::string& theme_name){}
-    virtual std::string GetTheme() { return "light"; }
+    virtual void SetTheme(const std::string& theme_name);
+    virtual std::string GetTheme() { return current_theme_name_; }
     virtual void UpdateBatteryPercentage(int percent);
 
     inline int width() const { return width_; }
