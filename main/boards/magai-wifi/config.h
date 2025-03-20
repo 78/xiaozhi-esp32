f915fb48dce2fd9d4df84fa0cb315ba0136559d5
666wsp <60884815+666wsp@users.noreply.github.com>
2025-03-20 23:47:39 +0800

    添加适配mag-ai板子

--- main/boards/magai-wifi/config.h
+++ main/boards/magai-wifi/config.h
@@ -3,7 +3,7 @@
 
 #include <driver/gpio.h>
 
-#define AUDIO_INPUT_SAMPLE_RATE         16000
+#define AUDIO_INPUT_SAMPLE_RATE         24000
 #define AUDIO_OUTPUT_SAMPLE_RATE        24000
 
 #define AUDIO_I2S_MIC_GPIO_WS           GPIO_NUM_4
