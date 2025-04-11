b952a24af89fd7c400e410afde4b0c5151b3d1e9
Suhayl <895761900@qq.com>
2025-04-11 15:45:44 +0800

    Format button.h to google C++ style.
    格式化button.h文件，与原来的保持一致

--- main/boards/common/button.h
+++ main/boards/common/button.h
@@ -5,33 +5,31 @@
 #include <button_gpio.h>
 #include <driver/gpio.h>
 #include <iot_button.h>
-
 #include <functional>
 
-
 class Button {
- public:
+public:
 #if CONFIG_SOC_ADC_SUPPORTED
-  Button(const button_adc_config_t &cfg);
+    Button(const button_adc_config_t& cfg);
 #endif
-  Button(gpio_num_t gpio_num, bool active_high = false);
-  ~Button();
+    Button(gpio_num_t gpio_num, bool active_high = false);
+    ~Button();
 
-  void OnPressDown(std::function<void()> callback);
-  void OnPressUp(std::function<void()> callback);
-  void OnLongPress(std::function<void()> callback);
-  void OnClick(std::function<void()> callback);
-  void OnDoubleClick(std::function<void()> callback);
+    void OnPressDown(std::function<void()> callback);
+    void OnPressUp(std::function<void()> callback);
+    void OnLongPress(std::function<void()> callback);
+    void OnClick(std::function<void()> callback);
+    void OnDoubleClick(std::function<void()> callback);
+private:
+    gpio_num_t gpio_num_;
+    button_handle_t button_handle_ = nullptr;
 
- private:
-  gpio_num_t gpio_num_;
-  button_handle_t button_handle_ = nullptr;
 
-  std::function<void()> on_press_down_;
-  std::function<void()> on_press_up_;
-  std::function<void()> on_long_press_;
-  std::function<void()> on_click_;
-  std::function<void()> on_double_click_;
+    std::function<void()> on_press_down_;
+    std::function<void()> on_press_up_;
+    std::function<void()> on_long_press_;
+    std::function<void()> on_click_;
+    std::function<void()> on_double_click_;
 };
 
-#endif  // BUTTON_H_
+#endif // BUTTON_H_
