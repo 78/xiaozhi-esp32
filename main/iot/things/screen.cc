c3e488e7447e1c463a680044ea06f4c711496515
Terrence <terrence@tenclass.com>
2025-05-19 10:06:05 +0800

    Iot: Use English description

--- main/iot/things/screen.cc
+++ main/iot/things/screen.cc
@@ -13,22 +13,22 @@ namespace iot {
 // 这里仅定义 Screen 的属性和方法，不包含具体的实现
 class Screen : public Thing {
 public:
-    Screen() : Thing("Screen", "这是一个屏幕，可设置主题和亮度") {
+    Screen() : Thing("Screen", "A screen that can set theme and brightness") {
         // 定义设备的属性
-        properties_.AddStringProperty("theme", "主题", [this]() -> std::string {
+        properties_.AddStringProperty("theme", "Current theme", [this]() -> std::string {
             auto theme = Board::GetInstance().GetDisplay()->GetTheme();
             return theme;
         });
 
-        properties_.AddNumberProperty("brightness", "当前亮度百分比", [this]() -> int {
+        properties_.AddNumberProperty("brightness", "Current brightness percentage", [this]() -> int {
             // 这里可以添加获取当前亮度的逻辑
             auto backlight = Board::GetInstance().GetBacklight();
             return backlight ? backlight->brightness() : 100;
         });
 
         // 定义设备可以被远程执行的指令
-        methods_.AddMethod("SetTheme", "设置屏幕主题", ParameterList({
-            Parameter("theme_name", "主题模式, light 或 dark", kValueTypeString, true)
+        methods_.AddMethod("set_theme", "Set the screen theme", ParameterList({
+            Parameter("theme_name", "Valid string values are \"light\" and \"dark\"", kValueTypeString, true)
         }), [this](const ParameterList& parameters) {
             std::string theme_name = static_cast<std::string>(parameters["theme_name"].string());
             auto display = Board::GetInstance().GetDisplay();
@@ -37,8 +37,8 @@ public:
             }
         });
         
-        methods_.AddMethod("SetBrightness", "设置亮度", ParameterList({
-            Parameter("brightness", "0到100之间的整数", kValueTypeNumber, true)
+        methods_.AddMethod("set_brightness", "Set the brightness", ParameterList({
+            Parameter("brightness", "An integer between 0 and 100", kValueTypeNumber, true)
         }), [this](const ParameterList& parameters) {
             uint8_t brightness = static_cast<uint8_t>(parameters["brightness"].number());
             auto backlight = Board::GetInstance().GetBacklight();
