37ee95c80dd21294a75833b07fcb555a24c6e2cb
mazhao <me@pawn-ma.com>
2025-06-05 17:44:52 +0800

    feat: 初步增加了硬件及引脚配置

---
+++ main/boards/fogseek-aus3-v1/fogseek-aus3-v1.cc
@@ -0,0 +1,58 @@
+#include "wifi_board.h"
+#include "audio_codecs/no_audio_codec.h"
+#include "system_reset.h"
+#include "application.h"
+#include "button.h"
+#include "config.h"
+#include "mcp_server.h"
+#include "lamp_controller.h"
+#include "iot/thing_manager.h"
+#include "led/single_led.h"
+#include "assets/lang_config.h"
+
+#include <wifi_station.h>
+#include <esp_log.h>
+
+
+#define TAG "FogSeekAus3V1"
+
+class FogSeekAus3V1 : public WifiBoard {
+private:
+    Button boot_button_;
+
+    void InitializeButtons() {
+        boot_button_.OnClick([this]() {
+            auto& app = Application::GetInstance();
+            if (app.GetDeviceState() == kDeviceStateStarting && !WifiStation::GetInstance().IsConnected()) {
+                ResetWifiConfiguration();
+            }
+            app.ToggleChatState();
+        });
+    }
+
+    // 物联网初始化，逐步迁移到 MCP 协议
+    void InitializeIot() {
+#if CONFIG_IOT_PROTOCOL_XIAOZHI
+        auto& thing_manager = iot::ThingManager::GetInstance();
+        thing_manager.AddThing(iot::CreateThing("Speaker"));
+        thing_manager.AddThing(iot::CreateThing("Lamp"));
+#elif CONFIG_IOT_PROTOCOL_MCP
+        static LampController lamp(LAMP_GPIO);
+#endif
+    }
+
+public:
+    FogSeekAus3V1() :
+        boot_button_(BOOT_BUTTON_GPIO) {
+        InitializeButtons();
+        InitializeIot();
+    }
+
+    virtual AudioCodec* GetAudioCodec() override {
+        static NoAudioCodecDuplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN);
+        return &audio_codec;
+    }
+};
+
+DECLARE_BOARD(FogSeekAus3V1);
