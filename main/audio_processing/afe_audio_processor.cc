d58037eecd488efbbf311596361c1df950c2ec83
Terrence <terrence@tenclass.com>
2025-06-08 02:39:53 +0800

    fix p4 compiling errors

--- main/audio_processing/afe_audio_processor.cc
+++ main/audio_processing/afe_audio_processor.cc
@@ -25,13 +25,18 @@ void AfeAudioProcessor::Initialize(AudioCodec* codec) {
     srmodel_list_t *models = esp_srmodel_init("model");
     char* ns_model_name = esp_srmodel_filter(models, ESP_NSNET_PREFIX, NULL);
 
-    afe_config_t* afe_config = afe_config_init(input_format.c_str(), NULL, AFE_TYPE_VC, AFE_MODE_LOW_COST);
-    afe_config->aec_mode = AEC_MODE_VOIP_LOW_COST;
+    afe_config_t* afe_config = afe_config_init(input_format.c_str(), NULL, AFE_TYPE_VC, AFE_MODE_HIGH_PERF);
+    afe_config->aec_mode = AEC_MODE_VOIP_HIGH_PERF;
     afe_config->vad_mode = VAD_MODE_0;
     afe_config->vad_min_noise_ms = 100;
-    afe_config->ns_init = true;
-    afe_config->ns_model_name = ns_model_name;
-    afe_config->afe_ns_mode = AFE_NS_MODE_NET;
+
+    if (ns_model_name != nullptr) {
+        afe_config->ns_init = true;
+        afe_config->ns_model_name = ns_model_name;
+        afe_config->afe_ns_mode = AFE_NS_MODE_NET;
+    } else {
+        afe_config->ns_init = false;
+    }
 
     afe_config->afe_perferred_core = 1;
     afe_config->afe_perferred_priority = 1;
