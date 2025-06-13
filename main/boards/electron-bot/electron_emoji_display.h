03c8ec6850a0c5128557c0e1d3dd1418cad00a39
txp <771454616@qq.com>
2025-06-13 13:41:22 +0800

    fix(ota): 修复 ottoRobot和electronBot OTA 升级崩溃问题 bug

--- main/boards/electron-bot/electron_emoji_display.h
+++ main/boards/electron-bot/electron_emoji_display.h
@@ -33,6 +33,9 @@ public:
     // 重写聊天消息设置方法
     virtual void SetChatMessage(const char* role, const char* content) override;
 
+    // 重写图标设置方法
+    virtual void SetIcon(const char* icon) override;
+
 private:
     void SetupGifContainer();
 
