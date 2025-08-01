27d1b724baa9d119e24f28ac78512927ea46b630
xuxin <xuxin@espressif.com>
2025-08-01 17:54:25 +0800

    feat: delete local assets

--- main/boards/echoear/emote_display.h
+++ main/boards/echoear/emote_display.h
@@ -5,7 +5,7 @@
 #include <functional>
 #include <esp_lcd_panel_io.h>
 #include <esp_lcd_panel_ops.h>
-#include "mmap_generate_emoji.h"
+#include "mmap_generate_emoji_normal.h"
 #include "gfx.h"
 
 namespace anim {
