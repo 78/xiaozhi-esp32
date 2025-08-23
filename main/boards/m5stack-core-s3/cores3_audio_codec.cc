7af366b7b260bcf35b83fec7e13b350dc069abb5
Terrence <terrence@tenclass.com>
2025-08-23 16:05:49 +0800

    fix: ES7120_SEL_MIC1 => ES7210_SEL_MIC1

--- main/boards/m5stack-core-s3/cores3_audio_codec.cc
+++ main/boards/m5stack-core-s3/cores3_audio_codec.cc
@@ -64,7 +64,7 @@ CoreS3AudioCodec::CoreS3AudioCodec(void* i2c_master_handle, int input_sample_rat
 
     es7210_codec_cfg_t es7210_cfg = {};
     es7210_cfg.ctrl_if = in_ctrl_if_;
-    es7210_cfg.mic_selected = ES7120_SEL_MIC1 | ES7120_SEL_MIC2 | ES7120_SEL_MIC3;
+    es7210_cfg.mic_selected = ES7210_SEL_MIC1 | ES7210_SEL_MIC2 | ES7210_SEL_MIC3;
     in_codec_if_ = es7210_codec_new(&es7210_cfg);
     assert(in_codec_if_ != NULL);
 
