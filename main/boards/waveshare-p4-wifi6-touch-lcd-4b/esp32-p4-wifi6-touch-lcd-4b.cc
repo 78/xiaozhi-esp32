a2202ee836573a976fa4fd67a0fce8ffa110f436
Y1hsiaochunnn <y1hsiaochunnn@qq.com>
2025-11-19 14:47:06 +0800

    Add Camera support to Waveshare's ESP32-P4 series development board

--- main/boards/waveshare-p4-wifi6-touch-lcd-4b/esp32-p4-wifi6-touch-lcd-4b.cc
+++ main/boards/waveshare-p4-wifi6-touch-lcd-4b/esp32-p4-wifi6-touch-lcd-4b.cc
@@ -6,6 +6,10 @@
 #include "button.h"
 #include "config.h"
 
+#include "esp32_camera.h"
+#include "esp_video_init.h"
+#include "esp_cam_sensor_xclk.h"
+
 #include "esp_lcd_panel_ops.h"
 #include "esp_lcd_mipi_dsi.h"
 #include "esp_ldo_regulator.h"
@@ -24,6 +28,7 @@ private:
     i2c_master_bus_handle_t i2c_bus_;
     Button boot_button_;
     LcdDisplay *display_;
+    Esp32Camera* camera_ = nullptr;
 
     void InitializeCodecI2c() {
         // Initialize I2C peripheral
@@ -149,6 +154,23 @@ private:
         lvgl_port_add_touch(&touch_cfg);
         ESP_LOGI(TAG, "Touch panel initialized successfully");
     }
+    void InitializeCamera() {
+        esp_video_init_csi_config_t base_csi_config = {
+            .sccb_config = {
+                .init_sccb = false,
+                .i2c_handle = codec_i2c_bus_,
+                .freq = 400000,
+            },
+            .reset_pin = GPIO_NUM_NC,
+            .pwdn_pin  = GPIO_NUM_NC,
+        };
+
+        esp_video_init_config_t cam_config = {
+            .csi      = &base_csi_config,
+        };
+
+        camera_ = new Esp32Camera(cam_config);
+    }
     void InitializeButtons() {
         boot_button_.OnClick([this]() {
             auto& app = Application::GetInstance();
@@ -164,6 +186,7 @@ public:
         InitializeCodecI2c();
         InitializeLCD();
         InitializeTouch();
+        InitializeCamera();
         InitializeButtons();
         GetBacklight()->RestoreBrightness();
     }
@@ -187,6 +210,10 @@ public:
 
     virtual Display *GetDisplay() override {
         return display_;
+    }
+
+    virtual Camera* GetCamera() override {
+        return camera_;
     }
 
     virtual Backlight* GetBacklight() override {
