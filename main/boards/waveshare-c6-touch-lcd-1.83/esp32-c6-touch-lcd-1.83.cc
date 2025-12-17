1a0c3ca35729a30a197d075247206c6e2d0aafcc
pfangzhi <69944031+pfangzhi@users.noreply.github.com>
2025-12-17 14:35:23 +0800

    Update esp32-c6-touch-lcd-1.83.cc

--- main/boards/waveshare-c6-touch-lcd-1.83/esp32-c6-touch-lcd-1.83.cc
+++ main/boards/waveshare-c6-touch-lcd-1.83/esp32-c6-touch-lcd-1.83.cc
@@ -9,8 +9,6 @@
 #include "power_save_timer.h"
 #include "axp2101.h"
 #include "i2c_device.h"
-#include <wifi_station.h>
-
 #include <esp_log.h>
 #include <esp_lcd_panel_vendor.h>
 #include <driver/i2c_master.h>
@@ -106,8 +104,9 @@ private:
     void InitializeButtons() {
         boot_button_.OnClick([this]() {
             auto& app = Application::GetInstance();
-            if (app.GetDeviceState() == kDeviceStateStarting && !WifiStation::GetInstance().IsConnected()) {
-                ResetWifiConfiguration();
+            if (app.GetDeviceState() == kDeviceStateStarting) {
+                EnterWifiConfigMode();
+                return;            
             }
             app.ToggleChatState();
         });
@@ -250,12 +249,12 @@ public:
         return true;
     }
 
-    virtual void SetPowerSaveMode(bool enabled) override {
-        if (!enabled)
+    virtual void SetPowerSaveLevel(PowerSaveLevel level) override {
+        if (level != PowerSaveLevel::LOW_POWER) {
         {
             power_save_timer_->WakeUp();
         }
-        WifiBoard::SetPowerSaveMode(enabled);
+       WifiBoard::SetPowerSaveLevel(level);
     }
 };
 
