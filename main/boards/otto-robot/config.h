645a25927cec971c5c8caa29d06efbdf49e9fdfd
txp <771454616@qq.com>
2026-02-01 22:00:19 +0800

    Enhance Otto Robot camera support by adding configuration for OV3660. Updated config.h to define camera types and GPIO settings, modified config.json to include new camera options, and refactored otto_robot.cc for improved camera detection and initialization logic.

--- main/boards/otto-robot/config.h
+++ main/boards/otto-robot/config.h
@@ -1,44 +1,63 @@
 #ifndef _BOARD_CONFIG_H_
 #define _BOARD_CONFIG_H_
 
-#include <driver/gpio.h>
 #include <driver/adc.h>
+#include <driver/gpio.h>
+
+#define OTTO_VERSION_AUTO 0
+#define OTTO_VERSION_CAMERA 1
+#define OTTO_VERSION_NO_CAMERA 2
+
+#ifndef OTTO_HARDWARE_VERSION
+#define OTTO_HARDWARE_VERSION OTTO_VERSION_AUTO
+#endif
+
+enum OttoCameraType {
+    OTTO_CAMERA_NONE = 0,
+    OTTO_CAMERA_OV2640 = 1,
+    OTTO_CAMERA_OV3660 = 2,
+    OTTO_CAMERA_UNKNOWN = 99,
+};
+
+#define OV2640_PID_1 0x2640
+#define OV2640_PID_2 0x2626
+#define OV3660_PID 0x3660
 
 struct HardwareConfig {
     gpio_num_t power_charge_detect_pin;
     adc_unit_t power_adc_unit;
     adc_channel_t power_adc_channel;
-    
+
     gpio_num_t right_leg_pin;
     gpio_num_t right_foot_pin;
     gpio_num_t left_leg_pin;
     gpio_num_t left_foot_pin;
     gpio_num_t left_hand_pin;
     gpio_num_t right_hand_pin;
-    
+
     int audio_input_sample_rate;
     int audio_output_sample_rate;
     bool audio_use_simplex;
-    
+
     gpio_num_t audio_i2s_gpio_ws;
     gpio_num_t audio_i2s_gpio_bclk;
     gpio_num_t audio_i2s_gpio_din;
     gpio_num_t audio_i2s_gpio_dout;
-    
+
     gpio_num_t audio_i2s_mic_gpio_ws;
     gpio_num_t audio_i2s_mic_gpio_sck;
     gpio_num_t audio_i2s_mic_gpio_din;
     gpio_num_t audio_i2s_spk_gpio_dout;
     gpio_num_t audio_i2s_spk_gpio_bclk;
     gpio_num_t audio_i2s_spk_gpio_lrck;
-    
+
     gpio_num_t display_backlight_pin;
     gpio_num_t display_mosi_pin;
     gpio_num_t display_clk_pin;
     gpio_num_t display_dc_pin;
     gpio_num_t display_rst_pin;
     gpio_num_t display_cs_pin;
-    
+
     gpio_num_t i2c_sda_pin;
     gpio_num_t i2c_scl_pin;
 };
@@ -47,37 +66,37 @@ constexpr HardwareConfig CAMERA_VERSION_CONFIG = {
     .power_charge_detect_pin = GPIO_NUM_NC,
     .power_adc_unit = ADC_UNIT_1,
     .power_adc_channel = ADC_CHANNEL_1,
-    
+
     .right_leg_pin = GPIO_NUM_43,
     .right_foot_pin = GPIO_NUM_44,
     .left_leg_pin = GPIO_NUM_5,
     .left_foot_pin = GPIO_NUM_6,
     .left_hand_pin = GPIO_NUM_4,
     .right_hand_pin = GPIO_NUM_7,
-    
+
     .audio_input_sample_rate = 16000,
     .audio_output_sample_rate = 16000,
     .audio_use_simplex = false,
-    
+
     .audio_i2s_gpio_ws = GPIO_NUM_40,
     .audio_i2s_gpio_bclk = GPIO_NUM_42,
     .audio_i2s_gpio_din = GPIO_NUM_41,
     .audio_i2s_gpio_dout = GPIO_NUM_39,
-    
+
     .audio_i2s_mic_gpio_ws = GPIO_NUM_NC,
     .audio_i2s_mic_gpio_sck = GPIO_NUM_NC,
     .audio_i2s_mic_gpio_din = GPIO_NUM_NC,
     .audio_i2s_spk_gpio_dout = GPIO_NUM_NC,
     .audio_i2s_spk_gpio_bclk = GPIO_NUM_NC,
     .audio_i2s_spk_gpio_lrck = GPIO_NUM_NC,
-    
+
     .display_backlight_pin = GPIO_NUM_38,
     .display_mosi_pin = GPIO_NUM_45,
     .display_clk_pin = GPIO_NUM_48,
     .display_dc_pin = GPIO_NUM_47,
     .display_rst_pin = GPIO_NUM_1,
     .display_cs_pin = GPIO_NUM_NC,
-    
+
     .i2c_sda_pin = GPIO_NUM_15,
     .i2c_scl_pin = GPIO_NUM_16,
 };
@@ -86,37 +105,37 @@ constexpr HardwareConfig NON_CAMERA_VERSION_CONFIG = {
     .power_charge_detect_pin = GPIO_NUM_21,
     .power_adc_unit = ADC_UNIT_2,
     .power_adc_channel = ADC_CHANNEL_3,
-    
+
     .right_leg_pin = GPIO_NUM_39,
     .right_foot_pin = GPIO_NUM_38,
     .left_leg_pin = GPIO_NUM_17,
     .left_foot_pin = GPIO_NUM_18,
     .left_hand_pin = GPIO_NUM_8,
     .right_hand_pin = GPIO_NUM_12,
-    
+
     .audio_input_sample_rate = 16000,
     .audio_output_sample_rate = 24000,
     .audio_use_simplex = true,
-    
+
     .audio_i2s_gpio_ws = GPIO_NUM_NC,
     .audio_i2s_gpio_bclk = GPIO_NUM_NC,
     .audio_i2s_gpio_din = GPIO_NUM_NC,
     .audio_i2s_gpio_dout = GPIO_NUM_NC,
-    
+
     .audio_i2s_mic_gpio_ws = GPIO_NUM_4,
     .audio_i2s_mic_gpio_sck = GPIO_NUM_5,
     .audio_i2s_mic_gpio_din = GPIO_NUM_6,
     .audio_i2s_spk_gpio_dout = GPIO_NUM_7,
     .audio_i2s_spk_gpio_bclk = GPIO_NUM_15,
     .audio_i2s_spk_gpio_lrck = GPIO_NUM_16,
-    
+
     .display_backlight_pin = GPIO_NUM_3,
     .display_mosi_pin = GPIO_NUM_10,
     .display_clk_pin = GPIO_NUM_9,
     .display_dc_pin = GPIO_NUM_46,
     .display_rst_pin = GPIO_NUM_11,
     .display_cs_pin = GPIO_NUM_12,
-    
+
     .i2c_sda_pin = GPIO_NUM_NC,
     .i2c_scl_pin = GPIO_NUM_NC,
 };
