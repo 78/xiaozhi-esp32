02b573e2f2af7f36fd0532128b419b841157e455
jeck.chen <jeck.chen@dbappsecurity.com.cn>
2026-02-04 19:31:25 +0800

    在Display基类中添加SetupUI虚函数
    Signed-off-by: jeck.chen <jeck.chen@dbappsecurity.com.cn>

--- main/display/display.h
+++ main/display/display.h
@@ -40,6 +40,7 @@ public:
     virtual Theme* GetTheme() { return current_theme_; }
     virtual void UpdateStatusBar(bool update_all = false);
     virtual void SetPowerSaveMode(bool on);
+    virtual void SetupUI() { }
 
     inline int width() const { return width_; }
     inline int height() const { return height_; }
