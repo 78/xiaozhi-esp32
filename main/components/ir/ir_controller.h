f2110af7c1cc83266d65c85c71c9b05233ae4d45
pzb568 <pengzb886@gmail.com>
2026-08-28 02:56:12 +0800

    ir: add header for IR controller (RMT NEC send/recv)

---
+++ main/components/ir/ir_controller.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+#include "esp_err.h"
+#include "driver/gpio.h"
+
+class IrController {
+public:
+    IrController(gpio_num_t rx_gpio, gpio_num_t tx_gpio, bool module_internal_modulator = true);
+    ~IrController();
+
+    esp_err_t SendNEC(uint16_t address, uint16_t command);
+    bool GetLastReceived(uint16_t &address, uint16_t &command);
+
+private:
+    gpio_num_t rx_gpio_;
+    gpio_num_t tx_gpio_;
+    bool module_internal_modulator_;
+
+    volatile uint16_t last_address_;
+    volatile uint16_t last_command_;
+    volatile bool last_valid_;
+
+    esp_err_t InitRmtTx();
+    esp_err_t InitRmtRx();
+    static void RmtRxTask(void* arg);
+    size_t BuildNecItems(uint16_t address, uint16_t command, rmt_item32_t* out_items, size_t max_items);
+
+    IrController(const IrController&) = delete;
+    IrController& operator=(const IrController&) = delete;
+};
